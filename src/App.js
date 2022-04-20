9f197cb46b597d0a2bd9b3438bfd961414a80356
Yeon Kim <awyeon@gmail.com>
2022-04-20 20:32:33 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-// import React from 'react'
-
-// function App() {
-//   return React.createElement(
-//     'div',
-//     { className: 'container' },
-//     React.createElement('h1', { }, 'My App')
-//   )
-// }
+import React from 'react'
 
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    {id: 1, text: 'Comment one'},
-    {id: 2, text: 'Comment two'},
-    {id: 3, text: 'Comment three'},
-  ]
-
   return (
     <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      <h1>haha</h1>
+      <p>hahah</p>
     </div>
   )
 }
