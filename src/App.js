bd31873c363f378072306a8e3097d56a0fc51ca8
Yeon Kim <awyeon@gmail.com>
2022-04-20 19:45:25 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+// import React from 'react'
+
+// function App() {
+//   return React.createElement(
+//     'div',
+//     { className: 'container' },
+//     React.createElement('h1', { }, 'My App')
+//   )
+// }
+
 function App() {
-  return <h1>Hello form the app component</h1>
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {id: 1, text: 'Comment one'},
+    {id: 2, text: 'Comment two'},
+    {id: 3, text: 'Comment three'},
+  ]
+
+  return (
+    <div className='container'>
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App
