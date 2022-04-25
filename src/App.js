a4c49fb962b3e575f548468a76bde579e8589a96
Yeon Kim <awyeon@gmail.com>
2022-04-25 16:16:43 +0800

    added proptypes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+// import Card from './components/shared/Card'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <div className='container'>
         <FeedbackList feedback={feedback} />
+        {/* <Card>Hello World</Card> */}
       </div>
     </>
   )
