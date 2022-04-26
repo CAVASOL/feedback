aae7a638767dd78353cec57f4980242143fa860c
Yeon Kim <awyeon@gmail.com>
2022-04-26 14:56:14 +0800

    added

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState, useContext, useEffect } from 'react'
 import RatingSelect from './RatingSelect'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackForm(handleAdd) {
+function FeedbackForm() {
   const [text, setText] = useState('')
   const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
+
+  const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext)
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
+  }, [feedbackEdit])
+
   const handleTextChange = (e) => {
     if(text === '') {
       setBtnDisabled(true)
@@ -26,37 +38,43 @@ function FeedbackForm(handleAdd) {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(text.trim().length > 10) {
+    if (text.trim().length > 10) {
       const newFeedback = {
         text,
         rating,
       }
 
-      handleAdd(newFeedback)
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+        addFeedback(newFeedback)
+      }
 
       setText('')
     }
   }
 
-  return <Card>
-    <form onSubmit={handleSubmit}>
-      <h2>How would you rate your service with us?</h2>
-      <RatingSelect select={setRating} selected={rating} />
-      <div className='input-group'>
-        <input
-          onChange={handleTextChange}
-          type='text'
-          placeholder='Write a review'
-          value={text}
-        />
-        <Button type='submit' isDisabled={btnDisabled}>
-          Send
-        </Button>
-      </div>
+  return (
+    <Card>
+      <form onSubmit={ handleSubmit }>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className='input-group'>
+          <input
+            onChange={ handleTextChange }
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
 
-      <div className='message'>{message}</div>
-    </form>
-  </Card>
+        <div className='message'>{message}</div>
+      </form>
+    </Card>
+  )
 }
 
 export default FeedbackForm
--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-// import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function RatingSelect({ select, selected }) {
-  // const [selected, setSelected] = useState(10)
+function RatingSelect({ select }) {
+  const [selected, setSelected] = useState(10)
+
+  const { feedbackEdit } = useContext(FeedbackContext)
+
+  useEffect(() => {
+    setSelected(feedbackEdit.item.rating)
+  }, [feedbackEdit])
 
   const handleChange = (e) => {
+    setSelected(+e.currentTarget.value)
     select(+e.currentTarget.value)
   }
 
