9e1e8954a213ed381829fe48d9bcf2dd275fb513
Yeon Kim <awyeon@gmail.com>
2022-04-26 15:29:10 +0800

    testing

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -32,7 +32,7 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete?')) {
+    if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
