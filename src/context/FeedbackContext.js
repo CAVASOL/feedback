769abbcf46916313732d0792a9ef51fda22ff286
Yeon Kim <awyeon@gmail.com>
2022-04-27 18:05:18 +0800

    delete and update

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -39,13 +39,25 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([data, ...feedback])
   }
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
+
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      header: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
+
     setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
   }
 
